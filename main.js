8e97f03f9e0eb06b419fec76b1eeacc34a814a2a
ArtemPonkratov <artemka.g@mail.ru>
2023-04-06 21:31:29 +0200

    part2

---
+++ main.js
@@ -0,0 +1,30 @@
+
+// number
+
+let num = 2;
+console.log(num);
+
+
+console.log('Добро ' + 'пожаловать ' + 'на курс' + 'JavaScript' + 'Artem');
+
+// function 
+
+alert(new Date); // текущую дату в формате "день.месяц.год"
+alert("06.04.2023");
+
+
+// html
+
+document.getElementById("one").innerText = "Hello World";
+
+
+
+
+console.log(typeof "Hello World"); // string
+console.log(typeof 2); // number
+console.log(typeof true); // boolean
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof []); // object
+console.log(typeof {}); // object
+console.log(typeof function(){}); // function
